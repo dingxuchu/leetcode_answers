9e4ceaa057fe89cf718eebcef110b36ff7db54fe
Dingxuchu <dingxc@zju.edu.cn>
2020-07-19 23:59:04 +0800

    fix bugs about leetcode answer 10

--- C++/10.cpp
+++ C++/10.cpp
@@ -9,6 +9,7 @@ public:
         size_t sSize = s.size();
         size_t pSize = p.size();
         std::vector<std::vector<bool>> result(sSize + 1, std::vector<bool>(pSize + 1, false));
+        result[0][0] = true;
         for (size_t i = 0; i <= sSize; ++i) {
             for (size_t j = 1; j <= pSize; ++j) {
                 if (p[j - 1] == '*') {
